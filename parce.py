dfd38c65d4f4175fd6c4ff7b8ee84b3dc7ec2333
naktu <dk@tutunak.com>
2015-11-16 20:11:24 +0300

    added simple parse tag a whith bs4

--- parce.py
+++ parce.py
@@ -1,10 +1,15 @@
-import urllib
+import urllib.request
 import sys
+from bs4 import BeautifulSoup
 
 
-def main():
-    
+def main(url):
+    html = urllib.request.urlopen(url).read()
+    soupPage = BeautifulSoup(html)
+    tags = soupPage('a')
+    for i in tags:
+        print (i.get('href', None))
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
