ee2d372c3bce9feb7149c160fe9f37a84a10f026
naktu <dk@tutunak.com>
2015-11-17 14:58:58 +0300

    get link for next page

--- eslpodParser.py
+++ eslpodParser.py
@@ -8,11 +8,17 @@ def main(url):
     html = urllib.request.urlopen(url).read()
     soupPage = BeautifulSoup(html, "lxml")
     tags = soupPage('a')
+    reSite = re.search("(^http.+)show_all.php", url)
+    site = reSite.group(1)
     for tag in tags:
         link = tag.get('href', None)
         if (re.match("^http.+mp3$", link)) != None:
             print(link)
+        elif (tag.contents[0] == "next"):
+            url = site + link
+    print(url)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    url = "https://www.eslpod.com/website/show_all.php"
+    main(url)
