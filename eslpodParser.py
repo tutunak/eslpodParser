bc9e44804cb9f8143e56a794532bc23e4b2eaad2
naktu <dk@tutunak.com>
2015-11-18 11:00:47 +0300

    added Logging

--- eslpodParser.py
+++ eslpodParser.py
@@ -3,9 +3,11 @@ import sys
 from bs4 import BeautifulSoup
 import re
 import time
+import logging
 
 
 def main(url):
+    logging.basicConfig(filename='eslpod.log',level=logging.INFO)
     while True:
         html = urllib.request.urlopen(url).read()
         soupPage = BeautifulSoup(html, "lxml")
@@ -16,12 +18,15 @@ def main(url):
         for tag in tags:
             link = tag.get('href', None)
             if (re.match("^http.+mp3$", link)) != None:
+                logging.info(link)
                 print(link)
             elif (tag.contents[0] == "next"):
                 url = site + link
+        logging.info(url)
         print(url)
         if prevUrl == url:
             break
+        logging.info('Pause 10 second')
         print("Pause 10 second")
         time.sleep(10)
 
