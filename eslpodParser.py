44f476939bf0e145be157e45a56cbb3d705f8e9f
naktu <dk@tutunak.com>
2015-11-18 10:47:16 +0300

    go to the next page

--- eslpodParser.py
+++ eslpodParser.py
@@ -2,22 +2,28 @@ import urllib.request
 import sys
 from bs4 import BeautifulSoup
 import re
+import time
 
 
 def main(url):
-    html = urllib.request.urlopen(url).read()
-    soupPage = BeautifulSoup(html, "lxml")
-    tags = soupPage('a')
-    reSite = re.search("(^http.+)show_all.php", url)
-    site = reSite.group(1)
-    for tag in tags:
-        link = tag.get('href', None)
-        if (re.match("^http.+mp3$", link)) != None:
-            print(link)
-        elif (tag.contents[0] == "next"):
-            url = site + link
-    print(url)
-
+    while True:
+        html = urllib.request.urlopen(url).read()
+        soupPage = BeautifulSoup(html, "lxml")
+        tags = soupPage('a')
+        reSite = re.search("(^http.+)show_all.php", url)
+        site = reSite.group(1)
+        prevUrl = url
+        for tag in tags:
+            link = tag.get('href', None)
+            if (re.match("^http.+mp3$", link)) != None:
+                print(link)
+            elif (tag.contents[0] == "next"):
+                url = site + link
+        print(url)
+        if prevUrl == url:
+            break
+        print("Pause 10 second")
+        time.sleep(10)
 
 if __name__ == '__main__':
     url = "https://www.eslpod.com/website/show_all.php"
