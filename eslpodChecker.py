b632b20fabf7a585d1df7e129de691275abebc27
naktu <dk@tutunak.com>
2015-11-19 15:19:35 +0300

    delted mistakes

--- eslpodChecker.py
+++ eslpodChecker.py
@@ -6,14 +6,16 @@ import re
 def main(url, lastCount):
     linkList = []
     html = urllib.request.urlopen(url).read()
-    soupPage = BeatifulSoup(html, "xml")
+    soupPage = BeautifulSoup(html, "lxml")
     tags = soupPage('a')
     for tag in tags:
         link = tag.get('href', None)
-        if (re.match("^http.+mp3$", link)) ! = None:
+        if (re.match("^http.+mp3$", link)) != None:
             linkList.append(link)
-        if len(linkList >= lastCount): break
+            print(link)
+        if len(linkList) >= lastCount: break
 
 if __name__ == '__main__':
     url = "https://www.eslpod.com/website/show_all.php"
     lastCount = 10
+    main(url, lastCount)
